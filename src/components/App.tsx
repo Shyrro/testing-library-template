0f1a416ddcafda996b7ca59a74b3f5cf26faa66d
Zakaria Sahmane <zakaria.sahmane@corp.ovh.com>
2022-07-12 10:33:17 +0200

    add test dialog not  working

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,25 +1,23 @@
+import { useRef, useLayoutEffect, useState } from 'react'
 import Avatar from 'components/Avatar'
 
 function App() {
+  const dialog = useRef<HTMLDialogElement>(null)
+  // const dialog: any = document.getElementById('dialogid')
+  const [open, setOpen] = useState(dialog?.current?.open)
+
+  useLayoutEffect(() => {
+    if (dialog.current) {
+      if (open) dialog?.current.show()
+      else dialog?.current.close()
+    }
+  }, [open])
+
   return (
-    <div className="bg-white">
-      <div className="mx-auto max-w-screen-xl py-16 px-4 sm:py-24 sm:px-6 lg:px-8">
-        <div className="text-center">
-          <h2 className="text-base font-semibold uppercase tracking-wide text-blue-600">
-            Welcome to
-          </h2>
-          <p className="my-3 text-4xl font-bold text-gray-900 sm:text-5xl sm:tracking-tight lg:text-6xl">
-            reactjs-vite-tailwindcss-boilerplate
-          </p>
-          <p className="text-xl text-gray-400">Start building for free.</p>
-          <p className="mt-5">
-            <Avatar
-              size="large"
-              src="https://www.gravatar.com/avatar/4405735f6f3129e0286d9d43e7b460d0"
-            />
-          </p>
-        </div>
-      </div>
+    <div className="bg-red">
+      {/* <dialog id="dialogid">Coucou</dialog> */}
+      <dialog ref={dialog}>Coucou</dialog>
+      <button onClick={() => setOpen(!open)}>toggle</button>
     </div>
   )
 }
--- src/components/test.tsx
+++ src/components/test.tsx
@@ -1,26 +1,14 @@
-import { render, screen } from '@testing-library/react'
+import { fireEvent, render, screen } from '@testing-library/react'
 
 import App from './App'
 
 describe('<App />', () => {
   it('should render the App', () => {
-    const { container } = render(<App />)
+    const app = render(<App />)
 
-    expect(
-      screen.getByRole('heading', {
-        name: /welcome to/i,
-        level: 2
-      })
-    ).toBeInTheDocument()
+    fireEvent.click(screen.getByRole('button'));
 
-    expect(
-      screen.getByText(/reactjs-vite-tailwindcss-boilerplate/i)
-    ).toBeInTheDocument()
+    expect(screen.getByText('Coucou')).toBeTruthy();
 
-    expect(screen.getByText(/start building for free./i)).toBeInTheDocument()
-
-    expect(screen.getByRole('img')).toBeInTheDocument()
-
-    expect(container.firstChild).toBeInTheDocument()
   })
 })
